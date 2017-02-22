09b147831c323d0c41cbd0a35e1fd8a7dea4974e
Steven Wooding <steve@stevenwooding.com>
2017-02-22 17:18:06 +0000

    feat: Completed ignore button display & clickable
    Ignore buttons are displayed for each loan.  A click event
    listener is then setup for each button once the page is loaded.
    Clicking the ignore button just prints the loan id to the console
    at the moment.

--- content.js
+++ content.js
@@ -1,2 +1,32 @@
-// Place an ignore button on each loan
-$(".loan-details").append("<button>Ignore</button>");
+/**
+ * Place an ignore button on each loan
+ */
+$('.loan-parts .loan-details').each(function(i, loanDetails) {
+  var $loanDetails = $(loanDetails),
+    loanId = $loanDetails.find('p.company-info').text().
+      replace(/\r?\n|\r/g, '');
+  $loanDetails.append('<button class="ignoreBtn" id="' + loanId +
+    '">Ignore</button>');
+});
+
+
+/**
+ * This function will interface with local storage to store the loan id to be
+ * ignored.
+ * @param {event} event The click event data that has the loan id stored in it.
+ */
+function ignoreLoan(event) {
+  console.log('Inside ignoreLoan function. loanId = ' + event.data.loanId);
+}
+
+
+/**
+ * Once the page has loaded, add a click event handler to each ignore button.
+ */
+$(document).ready(function() {
+  $('.ignoreBtn').each(function(i, btnDetails) {
+    var $btnDetails = $(btnDetails);
+    var loanId = $btnDetails.attr('id');
+    $btnDetails.click({ loanId: loanId }, ignoreLoan);
+  });
+});
